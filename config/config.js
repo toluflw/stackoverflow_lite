8ff0dde731d8313dd467e5f822eee733eba02df9
Tolu Sola-Adeyemi <tolusola-adeyemi@Tolus-MacBook-Pro.local>
2022-09-30 00:35:32 +0100

    god abeg

--- config/config.js
+++ config/config.js
@@ -2,11 +2,28 @@
 require('dotenv').config();
 
 module.exports = {
-  production: {
+  development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PWD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
-  }
+    dialect: process.env.DB_DIALECT
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PWD,
+    database: process.env.DB_TESTDB,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  },
+  production: {
+    use_env_variable: process.env.DATABASE_URL,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,  
+      },
+    },
+  },
 }
