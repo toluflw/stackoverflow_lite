aadf373343dca23e5dc26440af44d1b5dd55d53e
Tolu Sola-Adeyemi <tolusola-adeyemi@Tolus-MacBook-Pro.local>
2022-09-29 23:52:47 +0100

    dbprod

--- config/config.js
+++ config/config.js
@@ -2,22 +2,11 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
+  production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PWD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PWD,
-    database: process.env.DB_TESTDB,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-  },
-  production: {
-    use_env_variable: process.env.DATABASE_URL,
-    dialect: 'mysql'
-  },
+  }
 }
