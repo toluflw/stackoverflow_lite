4f1b8871977eff00a264cb8adb96a9c2d390adb0
toluwanimi sola-adeyemi <114409123+toluflw@users.noreply.github.com>
2022-09-29 21:47:45 +0100

    production_merge (#7)
    * db models created and migrated
    
    * user activities created (#1)
    
    * user activities created
    
    * user changes
    
    * user tests added
    
    * resolved changes in middleware and utils
    
    * user refactoring
    
    * changes made to utils
    
    * Question flow (#3)
    
    * user activities created
    
    * user changes
    
    * user tests added
    
    * authorization flow added
    
    * question flow first commit
    
    * Delete authController.js
    
    * Delete authRouter.js
    
    * Delete auth.spec.js
    
    * questions
    
    * question search flow
    
    * rebase
    
    * resolving conflicts
    
    * Update question.spec.js
    
    * Answer flow (#4)
    
    * user activities created
    
    * user changes
    
    * user tests added
    
    * authorization flow added
    
    * answer_flow
    
    * Delete authController.js
    
    * Delete authRouter.js
    
    * Delete .DS_Store
    
    * Delete auth.spec.js
    
    * voter_flow (#5)
    
    Co-authored-by: Tolu Sola-Adeyemi <tolusola-adeyemi@Tolus-MacBook-Pro.local>
    
    * Comment flow (#6)
    
    * voter_flow
    
    * comments_flow
    
    Co-authored-by: Tolu Sola-Adeyemi <tolusola-adeyemi@Tolus-MacBook-Pro.local>
    
    Co-authored-by: Tolu Sola-Adeyemi <tolusola-adeyemi@Tolus-MacBook-Pro.local>

--- routers/index.js
+++ routers/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/auth', require('./authRouter'));
 router.use('/users', require('./usersRouter'));
 router.use('/posts', require('./questionsRouter'));
 router.use('/posts/answers', require('./answersRouter'));
+router.use('/posts/answers/votes', require('./votesRouter'));
+router.use('/posts/answers/comments', require('./commentsRouter'));
 
 
 module.exports = router;
--- services/index.js
+++ services/index.js
@@ -2,10 +2,14 @@
 const usersService = require('./usersService');
 const questionsService = require('./questionsService');
 const answersService = require('./answersService');
+const votesService = require('./votesService');
+const commentsService = require('./commentsService');
 
 
 module.exports = {
   usersService,
   questionsService,
   answersService,
+  votesService,
+  commentsService,
 };
