cd127df86df6913f32b314e380c3cd5f05e27630
Tolu Sola-Adeyemi <tolusola-adeyemi@Tolus-MacBook-Pro.local>
2022-09-30 07:28:19 +0100

    sigh2

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     password: process.env.DB_PWD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: 'mysql'
   },
   test: {
     username: process.env.DB_USERNAME,
