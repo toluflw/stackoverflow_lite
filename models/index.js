6f07ea0a698f56c42515e8bc589d8919e2744643
Tolu Sola-Adeyemi <tolusola-adeyemi@Tolus-MacBook-Pro.local>
2022-09-30 07:38:55 +0100

    db url parsed

--- models/index.js
+++ models/index.js
@@ -14,6 +14,21 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+if (env === 'production') {
+  const { DATABASE_URL } = process.env;
+  const dbUrl = url.parse(DATABASE_URL);
+  const username = dbUrl.auth.substr(0, dbUrl.auth.indexOf(':'));
+  const password = dbUrl.auth.substr(
+    dbUrl.auth.indexOf(':') + 1,
+    dbUrl.auth.length
+  );
+  const dbName = dbUrl.path.slice(1);
+  const host = dbUrl.hostname;
+  const { port } = dbUrl;
+  config.host = host;
+  config.port = port;
+  sequelize = new Sequelize(dbName, username, password, config);
+}
 
 fs
   .readdirSync(__dirname)
