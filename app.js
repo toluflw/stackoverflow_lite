45fb2a3e62c85281271dbb555c8021901144245b
tolu sola-adeyemi <tolu.sola-adeyemi@flutterwavego.com>
2022-10-17 13:20:32 +0100

    db connection refactored

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const { sequelize } = require('./models');
-const index = require('./routers/index');
+const index = require('./routers');
 
 const app = express();
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //enables express to pass url encoded form format
+
+app.disable("x-powered-by");
 
 app.use('/api/v1/', index);
+
+
+// Handle cases where invalid JSON data is passed
+app.use((err, req, res, next) => {
+  if (err.type === 'entity.parse.failed') {
+    res.json({
+      status: 'error',
+      message: 'invalid JSON passed',
+      data: null,
+    });
+  } else {
+    next();
+  }
+});
 
 //catches all unassigned routes
 app.all('*', (req, res) => {
@@ -16,12 +33,5 @@ app.all('*', (req, res) => {
     });
   });
 
-
-//creates tables in the db from the models i.e syncs them
-app.listen({ port: 5000 }, async () =>{
-    console.log(`server up on port 5000`)
-    await sequelize.authenticate()
-    console.log('database connected')
-})
 
 module.exports = app;
