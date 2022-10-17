0d8b1a9e0f1499591908055b717a7a0c0ee77a47
Tolu Sola-Adeyemi <tolusola-adeyemi@Tolus-MacBook-Pro.local>
2022-10-17 11:07:11 +0100

    routers index.js fixed

--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
-
 const router = express.Router();
+const usersRouter = require('./usersRouter');
+const questionsRouter = require('./questionsRouter');
+const answersRouter = require('./answersRouter');
+const votesRouter = require('./votesRouter');
+const commentsRouter = require('./commentsRouter');
 
-router.use('/users', require('./usersRouter'));
-router.use('/posts', require('./questionsRouter'));
-router.use('/posts/answers', require('./answersRouter'));
-router.use('/posts/answers/votes', require('./votesRouter'));
-router.use('/posts/answers/comments', require('./commentsRouter'));
+
+
+router.use('/users', usersRouter);
+router.use('/posts', questionsRouter);
+router.use('/posts/answers', answersRouter);
+router.use('/posts/answers/votes', votesRouter);
+router.use('/posts/answers/comments', commentsRouter);
 
 
 module.exports = router;
